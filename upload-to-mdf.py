169b90d1cc6339e6be0ecfcbdd86b8e78db3012c
Jorge Esteves <jorgeestevesdev@gmail.com>
2022-10-06 15:44:49 +0100

    add upload to mdf using pyautogui and pywinauto

---
+++ upload-to-mdf.py
@@ -0,0 +1,65 @@
+""" py -m pip install pyautogui pywinauto """
+
+import os
+import glob
+import numpy as np
+from tracemalloc import stop
+from pyautogui import moveTo, center, locateOnScreen, click, press
+
+import pywinauto
+from pywinauto.application import Application
+
+
+""" env """
+path = "C:/Auto/mdf-images"
+
+def getImages():
+    global list_of_images
+
+    os.chdir(path)
+    list_of_images = glob.glob("*.png")
+    list_of_images = [x for x in list_of_images if not x.startswith('~$')]
+    list_of_images = np.sort(list_of_images)
+
+def clickImage(image):
+    moveTo(center(locateOnScreen(image)))
+    click()
+
+def getMDFWindow():
+    global app
+    try:
+        app = Application(backend="uia").connect(title_re=".*MDF.*")
+        return app.window(title_re=".*MDF.*").set_focus()
+    except:
+        print("MDF not found")
+        return None
+
+def insertDataToMDF():
+    """ insert data to MDF """
+    stop()
+    with open("NABM_TO_UPLOAD_*.txt", "r") as f:
+        for line in f:
+            """ get modal from app """
+            print(line)
+            """ press Enter """
+    press("enter")
+
+def main():
+    pywinauto.timings.Timings.after_clickinput_wait = 2
+    getMDFWindow()
+    getImages()
+
+    for (file, i) in set(list_of_images):
+        clickImage(file)
+        print(i)
+        print(app)
+
+        """ on step 4 write all nabm from txt file """
+        if i == 4: insertDataToMDF()
+
+        """ img 5 click each nabm row, fill in the data, press Enter """
+        """ click arrow up upload btn """
+        """ click green upload button """
+        """ select favorites > auto folder """
+
+main()
